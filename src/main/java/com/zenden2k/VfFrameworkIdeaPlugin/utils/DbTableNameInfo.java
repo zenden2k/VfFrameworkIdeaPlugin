05ec7b3906bfadc3153ecff70d62a4c1b5a92e2e
Sergey Svistunov <zenden2k@gmail.com>
2021-01-12 09:42:12 +0300

    Fixed Nullable annotation, changed plugin version, update changelog

--- src/main/java/com/zenden2k/VfFrameworkIdeaPlugin/utils/DbTableNameInfo.java
+++ src/main/java/com/zenden2k/VfFrameworkIdeaPlugin/utils/DbTableNameInfo.java
@@ -2,8 +2,7 @@ package com.zenden2k.VfFrameworkIdeaPlugin.utils;
 
 import com.intellij.openapi.util.TextRange;
 import org.jetbrains.annotations.NotNull;
-
-import javax.annotation.Nullable;
+import org.jetbrains.annotations.Nullable;
 
 public class DbTableNameInfo {
     private final String tableName;
